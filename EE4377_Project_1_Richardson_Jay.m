f956ecddf3eee765c665bb1e362accc7f425426e
Jay-Perry <Jay-Perry@users.noreply.github.com>
2015-02-23 11:57:56 -0600

    The truly updated version with subplots changed.

--- EE4377_Project_1_Richardson_Jay.m
+++ EE4377_Project_1_Richardson_Jay.m
@@ -9,10 +9,10 @@ A = 3; f= 1000; omega = 2*pi*f;
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
 x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
+subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
+subplot(4,1,2)
 u_t=heaviside(t);
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
